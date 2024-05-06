75a9272dc08a0d86f1eb572598e462558d06e6f9
Marco Antônio Calijorne Soares <marco.soares1@external.stellantis.com>
2024-05-06 08:59:25 -0300

    Commit inicial

--- README.md
+++
@@ -1,2 +0,0 @@
-# devMobileUDWMJ
-Projeto desenvolvido em conjunto para o aprendizado do desenvolvimento do Flutter/Dart e Android Studio
---
+++ myapp101/android/app/src/main/kotlin/com/example/myapp101/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myapp101
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
